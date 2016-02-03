f2309ce08abca4c1063b5206e6c30012e466a256
武田由美 <yumi.takeda.5j@gmail.com>
2016-02-03 17:19:15 +0900

    Initial Commit

---
+++ firstproj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstproj
+//
+//  Created by 武田由美 on 2016/02/03.
+//  Copyright © 2016年 武田由美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
